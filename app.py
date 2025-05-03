4031d627d01d68b31b709cf7f651d8a3b76371c9
Benchfish <43074506+Benchfish@users.noreply.github.com>
2025-05-04 00:26:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import time
 
-import redis
-from flask import Flask
+#import redis
+#from flask import Flask
 
-app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+app = allinone(__name__)
+#cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
     retries = 5
@@ -18,6 +18,6 @@ def get_hit_count():
             time.sleep(0.5)
 
 @app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+#def hello():
+#    count = get_hit_count()
+#    return 'Hello World! I have been seen {} times.\n'.format(count)
